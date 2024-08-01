b6e85f95ace1f1909f7cf027030f8ed5417cc4a4
Caine Merrick <cainemerrick@gmail.com>
2024-08-01 19:29:31 +0100

    simple routing set up - next handle auth

--- graft_frontend/src/App.js
+++ graft_frontend/src/App.js
@@ -1,13 +1,36 @@
 import './App.css';
 import Sidebar from './components/sidebar/sidebar';
 import TaskGraph from './components/reactflow/taskgraph';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function Graft(){
+  return(
+    <div className='App'>
+      <Sidebar/>
+      <TaskGraph/>
+    </div>
+  )
+}
+
+function Login(){
+  return(
+    <div>
+      <h1>Login</h1>
+      <input type='text' placeholder='username'/><br></br>
+      <input type='password' placeholder='password'/><br></br>
+      <button>GO</button>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <Sidebar></Sidebar>
-      <TaskGraph></TaskGraph>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login/>}></Route>
+        <Route path='taskgraph' element={<Graft/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
